6cc75245f457e6b57d6df97b013614db1b5ac999
Max Hernandez <mhern120@asu.edu>
2020-04-19 16:40:40 -0700

    seperated import and post user

--- ProjectTemplate/script.js
+++ ProjectTemplate/script.js
@@ -91,86 +91,68 @@ function logOff() {
     });
 }
 
-function postUser() {
+usersArray = [];
+function importUsers(){
     var webMethod = "ProjectServices.asmx/ImportUsers";
     $.ajax({
         type: "POST",
         url: webMethod,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
+        //TODO: If I can fix this, fix it. Probably not good to be doing synchronous webcalls
+        async: false,
         success: function (data) {
-            var usersArray = data.d;
-            console.log(usersArray);
-            //var usersID = localStorage.getItem('usersid');
-            var usersID = localStorage.getItem("UserId") - 1;
-            console.log(usersArray);
-            console.log(usersID);
-
-            firstName = usersArray[usersID]._FirstName;
-            document.getElementById("fName").innerHTML = firstName;
-
-            lastName = usersArray[usersID]._LastName;
-            document.getElementById("lName").innerHTML = lastName;
-
-            welcome = usersArray[usersID]._FirstName;
-            document.getElementById("welcomeFname").innerHTML = welcome;
-
-
-            myers = usersArray[usersID]._MyersBriggs;
-            document.getElementById("myersBriggs").innerHTML = myers;
-
-            hobbies = usersArray[usersID]._Hobbies;
-            document.getElementById("hobby1").innerHTML = hobbies;
-
-            goals = usersArray[usersID]._CareerGoals;
-            document.getElementById("goal1").innerHTML = goals;
-
-            jobTitle = usersArray[usersID]._JobTitle;
-            document.getElementById("jobTitle").innerHTML = jobTitle;
-
-            department = usersArray[usersID]._Department;
-            document.getElementById("jobDept").innerHTML = department;
-
-            //location = usersArray[usersID]._Location;
-            //document.getElementById("Location").innerHTML = location;
-
-            university = usersArray[usersID]._University;
-            document.getElementById("university").innerHTML = university;
-
-            gradDate = usersArray[usersID]._GradDate;
-            document.getElementById("gradYear").innerHTML = gradDate;
-
-            eduLevel = usersArray[usersID]._EdLevel;
-            document.getElementById("edLevel").innerHTML = eduLevel;
-
-            eduFocus = usersArray[usersID]._EdFocus;
-            document.getElementById("edFocus").innerHTML = eduFocus;
-
-            loc = usersArray[usersID]._Location;
-            document.getElementById("userLocation").innerHTML = loc;
-
-            // make sure there is two different locations job vs user location
-            jobLoc = usersArray[usersID]._Location;
-            document.getElementById("jobLocation").innerHTML = jobLoc;
-
-            availabilityType = usersArray[usersID]._AvailabilityType;
-            console.log(availabilityType);
-
-            for (i = 0; i < availabilityType.length; i++) {
-                $("input[name='availType']").filter(function () {
-                    return availabilityType[i].indexOf(this.value) != -1;
-                }).prop("checked", true);
-            }
-
-
-            availabilityTimes = usersArray[usersID]._AvailabilityTimes;
-            console.log(availabilityTimes);
-
-            for (i = 0; i < availabilityTimes.length; i++) {
-                $("input[name='availTime']").filter(function () {
-                    return availabilityTimes[i].indexOf(this.value) != -1;
-                }).prop("checked", true);
-            }
+            usersArray = data.d;
         }
-    });
+    })
+}
+
+function postUser() {
+
+    importUsers();
+
+    var usersID = localStorage.getItem("UserId") - 1;
+
+    // mapping from webcall to html page
+    firstName = usersArray[usersID]._FirstName;
+    document.getElementById("fName").innerHTML = firstName;
+    lastName = usersArray[usersID]._LastName;
+    document.getElementById("lName").innerHTML = lastName;
+    welcome = usersArray[usersID]._FirstName;
+    document.getElementById("welcomeFname").innerHTML = welcome;
+    myers = usersArray[usersID]._MyersBriggs;
+    document.getElementById("myersBriggs").innerHTML = myers;
+    hobbies = usersArray[usersID]._Hobbies;
+    document.getElementById("hobby1").innerHTML = hobbies;
+    goals = usersArray[usersID]._CareerGoals;
+    document.getElementById("goal1").innerHTML = goals;
+    jobTitle = usersArray[usersID]._JobTitle;
+    document.getElementById("jobTitle").innerHTML = jobTitle;
+    department = usersArray[usersID]._Department;
+    document.getElementById("jobDept").innerHTML = department;
+    university = usersArray[usersID]._University;
+    document.getElementById("university").innerHTML = university;
+    gradDate = usersArray[usersID]._GradDate;
+    document.getElementById("gradYear").innerHTML = gradDate;
+    eduLevel = usersArray[usersID]._EdLevel;
+    document.getElementById("edLevel").innerHTML = eduLevel;
+    eduFocus = usersArray[usersID]._EdFocus;
+    document.getElementById("edFocus").innerHTML = eduFocus;
+    loc = usersArray[usersID]._Location;
+    document.getElementById("userLocation").innerHTML = loc;
+    // make sure there is two different locations job vs user location
+    jobLoc = usersArray[usersID]._Location;
+    document.getElementById("jobLocation").innerHTML = jobLoc;
+    availabilityType = usersArray[usersID]._AvailabilityType;
+    for (i = 0; i < availabilityType.length; i++) {
+        $("input[name='availType']").filter(function () {
+            return availabilityType[i].indexOf(this.value) != -1;
+        }).prop("checked", true);
+    }
+    availabilityTimes = usersArray[usersID]._AvailabilityTimes;
+    for (i = 0; i < availabilityTimes.length; i++) {
+        $("input[name='availTime']").filter(function () {
+            return availabilityTimes[i].indexOf(this.value) != -1;
+        }).prop("checked", true);
+    }
 }
