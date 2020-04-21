d4da683128f4a9b55e7d384bbf92ceb386e5b7a8
juliapagliaro <julia.pagliaro@asu.edu>
2020-04-20 21:24:00 -0700

    updated attempt

---
+++ ProjectTemplate/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Connection
+    {
+        public string _MenteeId { get; set; }
+        public string[] _MentorId { get; set; }
+    }
+}
